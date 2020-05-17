b793aabaf12727dbca7d54fd11b04f5f32626287
Daniel Quintero <dgquintero02@hotmail.com>
2020-05-17 18:17:24 -0500

    Adding files 3-hbnb for html py and js for fetch places

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,44 @@
+/* Task 4 -Fetch places
+Based on 2-hbnb.js
+Send a POST request with Content-Type: application/json and an empty
+dictionary in the body - cURL version: curl "http://0.0.0.0:5001/api/v1/places_search"
+-XPOST -H "Content-Type: application/json" -d '{}'
+*/
+$(document).ready(function () {
+  const amens = {};
+  $('input:checkbox').click(function () {
+    $(this).each(function () {
+      if (this.checked) {
+        amens[$(this).data('id')] = $(this).data('name');
+      } else {
+        delete amens[$(this).data('id')];
+      }
+    });
+    if (Object.values(amens).length > 0) {
+      $('.amenities h4').text(Object.values(amens).join(', '));
+    } else {
+      $('.amenities h4').html('&nbsp');
+    }
+  });
+  $.get('http://127.0.0.1:5001/api/v1/status/', function (data, status) {
+    console.log(data);
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/places_search',
+    type: 'POST',
+    data: '{}',
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data) {
+      for (const content in data) {
+        const place = data[content];
+        $('.places ').append('<article><h2>' + place.name + '</h2><div class="price_by_night"><p>$' + place.price_by_night + '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
+      }
+    }
+  });
+});
